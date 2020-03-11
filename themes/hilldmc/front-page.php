29e2631cef928666447495a7d3fc051adecb7a44
hilldmc <hilldmc@gmail.com>
2020-03-11 14:52:31 -0700

    cleaning front-page of default content

--- themes/hilldmc/front-page.php
+++ themes/hilldmc/front-page.php
@@ -10,9 +10,21 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-    <h1 class="front-title">hillary mclean</h1>
+    <div class="title-box">
+      <h1 class="front-title">hillary mclean</h1>
+    </div>
 
+    <div class="projects-box">
+      <a href="<?php echo esc_url( home_url( '/portfolio' ) ); ?>" rel="portfolio">
+        <h2>the projects</h2>
+      </a>
+    <div>
 
+    <div class="person-box">
+      <a href="<?php echo esc_url( home_url( '/about' ) ); ?>" rel="about">
+        <h2>the person</h2>
+      </a>
+    </div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
