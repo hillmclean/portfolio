cc1862851f22f0dbbccfa5ea6e91922b025ff919
hilldmc <hilldmc@gmail.com>
2020-03-12 17:51:44 -0700

    blog grid mobile responsive but still needs style edits

--- themes/hilldmc/home.php
+++ themes/hilldmc/home.php
@@ -27,10 +27,7 @@ get_header(); ?>
 
 				<?php foreach($blog_posts as $post): setup_postdata ($post); ?>
 			
-			<div class="project-img">
-			<?php echo the_post_thumbnail()?>
-			</div>
-				
+
 			<div class="project-info">
 					<a href="<?php the_permalink()?>">
 					<h2><?php the_title() ?></h2>
@@ -38,6 +35,10 @@ get_header(); ?>
 					<p><?php the_content() ?></p>
 				<button class="journal-button"><a class="button-text" href="<?php the_permalink()?>">Read Entry</a>
 				</button>
+			</div>
+
+			<div class="project-img">
+			<?php echo the_post_thumbnail()?>
 			</div>
 
 	
